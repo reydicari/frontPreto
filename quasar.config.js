79bb13678ac6e9c8e2dbf46222f4230c158e7737
reydi <reydiangelcari@gmail.com>
2025-10-29 08:39:12 -0400

    hazh mode en vez de redirect

--- quasar.config.js
+++ quasar.config.js
@@ -37,7 +37,7 @@ export default defineConfig((ctx) => {
         node: "node20",
       },
 
-      vueRouterMode: "history", // available values: 'hash', 'history'
+      vueRouterMode: "hash", // available values: 'hash', 'history'
       // vueRouterBase,
       // vueDevtools,
       // vueOptionsAPI: false,
